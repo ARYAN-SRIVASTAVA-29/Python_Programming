feecc9c19a9ed5cd7b197c8a8744e549dfc5a164
Aryan srivastava <mainaryansrivastav@gmail.com>
2024-06-02 17:58:11 +0530

    Variable and Datatypes

---
+++ Variables_DataTypes/FirstProgram.py
@@ -0,0 +1,28 @@
+print("Aryan Srivastava")
+print("My age is 19")
+
+# ==================================
+
+name = "Aryan"
+age = 23
+price = 25.00
+
+print("My name is :", name)
+print("my age is :", age, "Milk price is: ", price)
+
+# ==================================
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+# ===============================
+
+a = 200
+b = 50
+sum = a + b
+print("The sum is :", sum)
+
+# ==============================
+
+
---
+++ Variables_DataTypes/SecondProgram.py
@@ -0,0 +1,4 @@
+name = input("Enter Name:")
+age = int(input("Enter age:"))
+
+print("My name is", name, "Age is", age)

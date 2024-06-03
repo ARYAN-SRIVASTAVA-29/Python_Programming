aee493e407ed171e0e13062293770ae433f1e4b8
Aryan srivastava <mainaryansrivastav@gmail.com>
2024-06-03 12:21:59 +0530

    Variables and Data Types

--- Variables_DataTypes/SecondProgram.py
+++ Variables_DataTypes/SecondProgram.py
@@ -2,3 +2,28 @@ name = input("Enter Name:")
 age = int(input("Enter age:"))
 
 print("My name is", name, "Age is", age)
+
+# ================================
+
+# arithmetic operator
+a = 5
+b = 2
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b) #remainder
+print(a ** b) #a^b
+
+
+#relational operators
+a = 50
+b = 20
+
+print(a == b) #False
+print(a != b) #True
+print(a >= b) #True
+print(a > b) #True
+print(a <= b) #False
+print(a < b) #False
